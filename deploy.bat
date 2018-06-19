f052c9370309c9743c3196bad4df69d21bdca33a
stba <mail@stefanbaust.de>
2018-06-19 21:26:15 +0200

    session

--- deploy.bat
+++ deploy.bat
@@ -5,3 +5,4 @@ robocopy private-session/ dist/private-session/ /s /e
 robocopy vendor/ dist/vendor/ /s /e
 copy *.html dist
 ncftpput -R -v -u "stefanbaust@spltscreen.de" esm19.siteground.biz / dist/*
+TODO TRAVIS-CI
