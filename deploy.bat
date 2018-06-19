0a1563ba14f1164f19b3dc6f22ad1a0c9fb6c372
stba <mail@stefanbaust.de>
2018-06-19 21:25:39 +0200

    session

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+robocopy css/ dist/css/ /s /e
+robocopy img/ dist/img/ /s /e
+robocopy js/ dist/js/ /s /e
+robocopy private-session/ dist/private-session/ /s /e
+robocopy vendor/ dist/vendor/ /s /e
+copy *.html dist
+ncftpput -R -v -u "stefanbaust@spltscreen.de" esm19.siteground.biz / dist/*
